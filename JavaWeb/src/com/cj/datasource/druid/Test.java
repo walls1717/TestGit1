a8120ec3d38350d6aa7db46eac0f8a813b3c39c1
walls1717 <1067766124@qq.com>
2020-05-28 00:07:43 +0800

    Test

---
+++ JavaWeb/src/com/cj/datasource/druid/Test.java
@@ -0,0 +1,11 @@
+package com.cj.datasource.druid;
+
+/**
+ * 测试git 是否推送正常
+ * @author CJ
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git Push...");
+    }
+}
