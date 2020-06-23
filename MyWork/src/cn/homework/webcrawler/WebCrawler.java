c6e69d4daf9f8584650ec2f4b5e7dde74bc952b2
walls1717 <1067766124@qq.com>
2020-06-23 14:21:12 +0800

    Test

--- MyWork/src/cn/homework/webcrawler/WebCrawler.java
+++ MyWork/src/cn/homework/webcrawler/WebCrawler.java
@@ -1,11 +1,10 @@
 package cn.homework.webcrawler;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -18,25 +17,43 @@ import java.util.regex.Pattern;
  */
 public class WebCrawler {
 
-    private String webUrl;
+    private final String webUrl;
 
+    /**
+     * 在创建对象的时候，传入 url 地址
+     * @param webUrl 传入的 url 地址
+     */
     public WebCrawler(String webUrl) {
         this.webUrl = webUrl;
     }
 
     public void getUrl() {
+        // 待处理的 url
         ArrayList<String> list1 = new ArrayList<>();
+        // 将要输出的 url
         ArrayList<String> list2 = new ArrayList<>();
 
+        // 将输入的 url 地址添加到 list1 中
         list1.add(webUrl);
-        while (!list1.isEmpty() && list2.size() <= 20) {
-            String urlString = list1.remove(0);
 
+        // 需要输出多少条 url
+        int length = 20;
+
+        // 判断输入的 url 是否为空，一共输出 20 条 url 信息。
+        while (!list1.isEmpty() && list2.size() <= length) {
+            // 将输入的 url 存入 urlSting 中，并移除。
+            String urlString = list1.remove(0);
+            System.out.println("list1.size：" + list1.size());
+
+            // 如果 list2 中不包含这条输入的 url
             if (!list2.contains(urlString)) {
+                // 将输入的 url 放入 list2 中
                 list2.add(urlString);
                 System.out.println("发现URL：" + urlString);
 
+                // 每有一条新的 url，就会遍历这条 url 里面的所有 url
                 for (String s : getSubUrl2(urlString)) {
+                    // 如果 list2 中不包含这条 url，那么将这条 url 存入 list1 中。
                     if (!list2.contains(s)) {
                         list1.add(s);
                     }
@@ -98,7 +115,7 @@ public class WebCrawler {
             conn.setRequestMethod("GET");
             conn.setRequestProperty(
                     "User-Agent"," Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363");
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             String msg = null;
             while (null != (msg = br.readLine())) {
                 Pattern p = Pattern.compile(regex);
